1f43cc12d72857e94670bf2f8c50a5be53b14d25
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-10-24 19:38:59 -0400

    chore: remove nested git repository from Daily_Motivation

---
+++ Daily_Motivation/Daily_Motivations/Daily_Motivations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Daily_Motivations
+//
+//  Created by Christopher Wilshusen on 10/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Daily_Motivation/Daily_Motivations/Daily_Motivations/Daily_MotivationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_MotivationsApp.swift
+//  Daily_Motivations
+//
+//  Created by Christopher Wilshusen on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Daily_MotivationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
