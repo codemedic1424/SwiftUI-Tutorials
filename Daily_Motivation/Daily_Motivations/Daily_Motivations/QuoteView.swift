0682ad59b4239b147e9523ec531ca5e4461de83f
codemedic1424 <sunnier.stopper.0x@icloud.com>
2025-10-24 23:15:03 -0400

    feat: created initial view and added quoteview for reusability. Complete week 1.

---
+++ Daily_Motivation/Daily_Motivations/Daily_Motivations/QuoteView.swift
@@ -0,0 +1,18 @@
+//
+//  QuoteView.swift
+//  Daily_Motivations
+//
+//  Created by Christopher Wilshusen on 10/24/25.
+//
+
+import SwiftUI
+
+struct QuoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuoteView()
+}
