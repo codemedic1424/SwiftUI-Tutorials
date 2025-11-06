a1a9467655841370fba1c92b9124062b224e5a66
Christopher <sunnier.stopper.0x@icloud.com>
2025-11-06 14:53:29 -0500

    Add guided projects

---
+++ Guided Projects/CommonInputControls/CommonInputControls/CommonInputControls/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CommonInputControls
+//
+//  Created by Christopher Wilshusen on 11/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("Button was tapped!")
+    }
+    
+}
+
---
+++ Guided Projects/Hello/Hello/Hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Christopher Wilshusen on 11/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
